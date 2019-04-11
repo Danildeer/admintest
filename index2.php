8622b53fb454278da5de28cedcd479873d65aa3d
Danildeer <Danildeer@yandex.ru>
2019-04-12 01:39:08 +0400

    check2

--- index2.php
+++ index2.php
@@ -6,15 +6,15 @@
 </head>
 <body>
 	<div style="width: 100%; height: 300px;background-color: green;">
-		<a href="index2.php">
-			<h5>Для зарегистрированных пользователей</h5>
+		<a href="index1.php">
+			<h5>Для посетителей</h5>
 		</a>
 		<a href="index3.php">
 			<h5>Для админа</h5>
 		</a>
 	</div>
     <div style="width: 100%; height: 300px;background-color: red;">
-    	<h1>Страница посетителей</h1>
+    	<h1>Страница  зарегистрированных пользователей</h1>
     </div>
     <div style="width: 100%; height: 300px;background-color: yellow;"></div>
 </body>
